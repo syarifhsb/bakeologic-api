9171d383c6337840abd183321ae6f4520004332d
syarifhasibuan <syarifhsb.dev@gmail.com>
2025-03-07 20:01:42 +0100

    feat: add dbml generator fix: prisma schema slug unique

---
+++ prisma/migrations/20250307184123_add_slug_and_imageurl/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[slug]` on the table `Category` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `slug` to the `Category` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `imageUrl` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `slug` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "slug" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "imageUrl" TEXT NOT NULL,
+ADD COLUMN     "slug" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_slug_key" ON "Category"("slug");
