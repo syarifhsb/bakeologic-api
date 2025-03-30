314ba4898107d6f30e0e2b9ee4087d900a320aef
syarifhasibuan <syarifhsb.dev@gmail.com>
2025-03-31 00:59:45 +0200

    feat: add stock quantity to product schema closes BKE-42

--- src/modules/product/data.ts
+++ src/modules/product/data.ts
@@ -11,6 +11,7 @@ export const seedDataProducts: SeedProduct[] = [
     price: 2.5,
     categorySlug: "pastries",
     featured: true,
+    stockQuantity: 15,
     images: [
       {
         url: "https://ucarecdn.com/702dc96c-5b05-45fd-878e-d6914f677929/-/preview/612x408/",
@@ -23,6 +24,7 @@ export const seedDataProducts: SeedProduct[] = [
     name: "Flan Nature",
     price: 3.25,
     categorySlug: "pastries",
+    stockQuantity: 15,
     images: [
       {
         url: "https://ucarecdn.com/21cd6c76-7f7c-4b09-9346-76be1c7d0aca/-/preview/612x408/",
@@ -37,6 +39,7 @@ export const seedDataProducts: SeedProduct[] = [
     price: 2.75,
     categorySlug: "pastries",
     featured: true,
+    stockQuantity: 15,
     images: [
       {
         url: "https://ucarecdn.com/fa94e535-9a0f-4248-8948-455ac571a9fc/-/preview/612x408/",
@@ -49,6 +52,7 @@ export const seedDataProducts: SeedProduct[] = [
     name: "Bakeologics Cappuccino",
     price: 4.5,
     categorySlug: "beverages",
+    stockQuantity: 15,
     images: [
       {
         url: "https://ucarecdn.com/f1ae429c-3efc-464c-abcf-ab6b59a2c0e4/-/preview/612x408/",
@@ -62,6 +66,7 @@ export const seedDataProducts: SeedProduct[] = [
     price: 2.25,
     categorySlug: "breads",
     featured: true,
+    stockQuantity: 15,
     images: [
       {
         url: "https://ucarecdn.com/70fb616c-88e2-4f1c-9436-e36371a8ab18/-/preview/612x408/",
@@ -74,6 +79,7 @@ export const seedDataProducts: SeedProduct[] = [
     name: "Chicken Sandwich",
     price: 8.95,
     categorySlug: "sandwiches",
+    stockQuantity: 15,
     images: [
       {
         url: "https://ucarecdn.com/94f7b6dd-934d-4d40-996e-b8f9984f8dda/-/preview/612x408/",
