0987d04e568fd24f4667e5b47aca259dba3d1697
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-27 12:18:38 +0700

    feat: update PriceSchema to support multiple value types and enforce minimum price validation

--- src/modules/common/schema.ts
+++ src/modules/common/schema.ts
@@ -7,7 +7,19 @@ export const ResponseErrorSchema = z.object({
 });
 
 export const PriceSchema = z
-  .instanceof(Prisma.Decimal)
-  .refine((value) => value.gte("0.01"));
+  .union([z.string(), z.number(), z.instanceof(Prisma.Decimal)])
+  .refine((value) => {
+    if (typeof value === "number") {
+      return value >= 0.01; // 1 cent
+    }
+    if (typeof value === "string") {
+      const parsedValue = parseFloat(value);
+      return !isNaN(parsedValue) && parsedValue >= 0.01; // 1 cent
+    }
+    if (value instanceof Prisma.Decimal) {
+      return value.gte(new Prisma.Decimal(0.01)); // 1 cent
+    }
+    return false;
+  });
 
 export const UpsertPriceSchema = z.number().refine((value) => value >= 0.01);
